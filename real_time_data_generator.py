dc83f989b102277e541474ebadee8ecc927b2058
Nurkyial <nurkiyalalmazova@gmail.com>
2024-10-22 10:57:57 +0300

    added fuctions for creating csv to real_time_data_generator

--- real_time_data_generator.py
+++ real_time_data_generator.py
@@ -21,13 +21,39 @@ while True:
                 client.add_transaction(transaction)
                 client.add_payment(payment)
     timestamp = fun.datetime.now().strftime("%Y%m%d_%H%M%S")
+    client.generate_anomalous_data()
     data_logins = []
     for _ in client.logins:
         data_logins.append(_.login_data_to_dict())
-    login_df = fun.pd.DataFrame(data_logins)
-    login_df.to_csv(f'client_logins_{timestamp}.csv', index=False)
-    client.generate_anomalous_data()
+    data_df = fun.pd.DataFrame(data_logins)
+    data_df.to_csv(f'client_logins_{timestamp}.csv', index=False)
+
+    data_activities = []
+    for _ in client.activities:
+        data_activities.append(_.activity_data_to_dict())
+    data_df = fun.pd.DataFrame(data_activities)
+    data_df.to_csv(f'client_activities_{timestamp}.csv', index=False)
+
+    data_transactions = []
+    for _ in client.transactions:
+        data_transactions.append(_.transaction_data_to_dict())
+    data_df = fun.pd.DataFrame(data_transactions)
+    data_df.to_csv(f'client_transactions_{timestamp}.csv', index=False)
+
+    data_payments = []
+    for _ in client.payments:
+        data_payments.append(_.payment_data_to_dict())
+    data_df = fun.pd.DataFrame(data_payments)
+    data_df.to_csv(f'client_payments_{timestamp}.csv', index=False)
+
+
+
+    
+    
+
     break
+
+
 
 for _ in client.payments:
     print(_.payment_data_to_dict())
