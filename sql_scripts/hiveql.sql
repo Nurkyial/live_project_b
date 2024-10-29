8bcd00dfac0c2b916da6f23858f01929f38927eb
Nurkyial <nurkiyalalmazova@gmail.com>
2024-10-29 10:58:27 +0300

    finished sql script and started uploading data from hdfs

--- sql_scripts/hiveql.sql
+++ sql_scripts/hiveql.sql
@@ -3,22 +3,80 @@ show databases;
 
 use live_project_b;
 
-CREATE EXTERNAL TABLE IF NOT EXISTS clients (
+CREATE EXTERNAL TABLE IF NOT EXISTS clients_activities (
     client_id INT,
-    client_name STRING,
-    client_email STRING,
-    client_phone STRING,
-    client_address STRING
+    activity_date TIMESTAMP,
+    activity_type STRING,
+    activity_location STRING,
+    ip_address STRING,
+    device STRING
 )
-ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
-WITH SERDEPROPERTIES (
-    "separatorChar" = ",",
-    "quoteChar" = "\"",
-    "escapeChar" = "\\"
-)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
-LOCATION '/user/live_project_b';
+LOCATION '/user/live_project_b/clients_activities';
 
-select * from clients;
+select * from clients_activities;
 
-drop table if exists clients;
+drop table if exists clients_payments;
+
+
+CREATE EXTERNAL TABLE IF NOT EXISTS clients_calls_support (
+    client_id INT,
+    call_date TIMESTAMP,
+    duration INT,
+    result STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION '/user/live_project_b/clients_calls_support';
+
+select * from clients_calls_support;
+
+
+CREATE EXTERNAL TABLE IF NOT EXISTS clients_logins (
+    client_id INT,
+    login_date TIMESTAMP,
+    ip_address STRING,
+    location STRING,
+    device STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION '/user/live_project_b/clients_logins';
+
+select * from clients_logins;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS clients_payments (
+    client_id INT,
+    payment_id INT,
+    payment_date TIMESTAMP,
+    currency STRING,
+    amount DECIMAL(12, 2),
+    payment_method STRING,
+    transaction_id INT
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION '/user/live_project_b/clients_payments';
+
+select * from clients_payments;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS clients_transactions (
+    client_id INT,
+    transaction_id INT,
+    transaction_date TIMESTAMP,
+    transaction_type STRING,
+    account_number STRING,
+    currency STRING,
+    amount DECIMAL(12, 2)
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+LOCATION '/user/live_project_b/clients_transactions';
+
+select * from clients_transactions;
