ee4640035d940fb5996613517a17670db4fb4f50
Nurkyial <nurkiyalalmazova@gmail.com>
2024-11-11 14:46:26 +0300

    created function for calculating statistics (yet only max)

--- sql_scripts/functions.sql
+++ sql_scripts/functions.sql
@@ -10,3 +10,55 @@ BEGIN
 END;
 $$
  LANGUAGE plpgsql;
+
+ -- Function for calculating maximum, minimum, average, median, mode of the given columns
+CREATE OR REPLACE FUNCTION calculate_statistics(
+	table_name TEXT, 
+	column_name_agg TEXT, 
+	transactionType_or_paymentMethod TEXT, 
+	condition_value INT, 
+	currency_column TEXT, 
+	currency_condition INT,
+	function_type TEXT,
+	client_id INT
+	)
+RETURNS INT AS $$
+DECLARE 
+	-- results INTEGER[] := '{}';
+	resultat INT;
+	query TEXT;
+BEGIN 
+	IF function_type = 'MAX' THEN
+		    query := ' SELECT ' ||  function_type || '(' || column_name_agg || ') 
+			FROM ' || table_name ||			
+			' WHERE ' ||  transactionType_or_paymentMethod || ' = $1 AND '
+			|| currency_column || ' = $2 AND client_id = $3' 
+			' GROUP BY client_id';
+--			query := 'SELECT $7($2) FROM $1
+--					  WHERE $3 = $4 
+--					  AND $5 = $6 
+--					  AND client_id = $8
+--					  GROUP BY client_id';
+--		EXECUTE query INTO resultat USING table_name, column_name_agg, transactionType_or_paymentMethod,  
+--				 condition_value, currency_column, currency_condition, function_type, client_id;	
+		EXECUTE query INTO resultat USING condition_value, currency_condition, client_id;
+	END IF;
+	RETURN resultat;
+END;
+$$
+ LANGUAGE plpgsql;
+DROP FUNCTION calculate_statistics;
+SELECT calculate_statistics(
+	'transactions', 
+	'amount',
+	'transaction_type_id',
+	1,
+	'currency_id',
+	1,
+	'MAX',
+	1);
+
+SELECT amount FROM transactions
+WHERE transaction_type_id = 1 AND currency_id = 1 AND client_id = 1
+ORDER BY amount DESC
+LIMIT 10;
